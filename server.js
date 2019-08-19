d8a0782ffa2197e3a7e9ab8891973eb5894f18f9
Aijah Chappell <aijahc@comcast.net>
2019-08-19 16:32:31 -0400

    added code to load routes, start server, and listen in server.js

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const path = require("path");
 const htmlRoutes = require("./app/routing/htmlRoutes");
 const apiRoutes = require("./app/routing/apiRoutes");
 const friends = require("./app/data/friends");
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("Server started and listening on port: " + PORT);
+});
