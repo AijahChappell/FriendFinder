da2e3036039eb19df0e8037775f124fac952d393
Aijah Chappell <aijahc@comcast.net>
2019-08-22 10:20:22 -0400

    fixed routing issues

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,13 +1,17 @@
 const path = require("path");
+const express=require("express");
 
 module.exports = function(app) {
 
     app.get("/", function(req,res) {
         res.sendFile(path.join(__dirname, "/../public/home.html"));
     });
-    app.get("/survey", function(req,res) {
+    app.get("/survey.html", function(req,res) {
         res.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
+
+    app.use(express.static(path.join(__dirname,"/../public"))
+    );
 
 // app.use(function(req, res){
 //     res.sendFile(path.join(__dirname, "/../public/home.html"));
