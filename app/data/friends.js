186b396c1dcc06487fbb5ab933057e27925a707f
Aijah Chappell <aijahc@comcast.net>
2019-08-22 10:38:06 -0400

    fixed routing issues

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,20 +2,20 @@ const userData = [
     {
         name: "Kelly Kolding",
         photo: "https://unsplash.com/photos/mEZ3PoFGs_k",
-        scores: ["2", "2", "2", "2", "2"]
+        scores: ["1", "2", "3", "4", "5", "5", "4", "3", "2", "1"]
     }, {
         name: "Jessica Lindt",
         photo: "https://unsplash.com/photos/u3WmDyKGsrY",
-        scores: ["3", "3", "3", "3", "3"]
+        scores: ["3", "4", "5", "1", "2", "3", "1", "5", "4", "2"]
     }, {
 
         name: "David McKay",
         photo: "https://unsplash.com/photos/_YQgJos0SKE",
-        scores: ["1", "1", "1", "1", "1"]
+        scores: ["2", "4", "1", "3", "5", "4", "2", "3", "1", "5"]
     }, {
         name: "Daniel Bernard",
         photo: "https://unsplash.com/photos/kmxvl3HlTqw",
-        scores: ["3", "3", "3", "3", "1"]
+        scores: ["5", "4", "3", "2", "1", "3", "2", "1", "5", "4"]
     },
 ];
 
