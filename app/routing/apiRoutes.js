91d9d53113a1f8db87a722d4fe11f10c1447d036
Aijah Chappell <aijahc@comcast.net>
2019-08-21 20:08:53 -0400

    code for apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,7 +8,25 @@ module.exports = function(app){
     app.post("/api/friends", function(req, res) {
         const newFriend = req.body;
         const scoreArr = [];
-        let 
+        let count = 0;
+        let match = 0;
+
+        for(let i=0; i<userData.length; i++) {
+            let difference = 0;
+            for(let j=0; j<newFriend.scores.length; j++) {
+                difference += (Math.abs(parseInt(userData[i].scores[j])))
+            }
+            scoreArr.push(difference);
+        }
+        for(let k=0; k<scoreArr.length; k++) {
+            if(scoreArr[k] <= scoreArr[match]){
+                match=k;
+            }
+        }
+
+        let newBestFriend=userData[match];
+        res.json(newBestFriend);
+        userData.push(req.body);
     })
 
 }
--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const bodyParser = require ("body-parser");
 const path = require("path");
-const htmlRoutes = require("./app/routing/htmlRoutes");
-const apiRoutes = require("./app/routing/apiRoutes");
-const friends = require("./app/data/friends");
+
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-app.use(bodyParser.json());
 app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 app.listen(PORT, function() {
     console.log("Server started and listening on port: " + PORT);
